e82c36b7f77d4aeb9089484184d9ae970dd8782f
Zan Kavtaskin <kavtaskinz@MSL2487.midland-software.co.uk>
2020-04-25 23:42:11 +0100

    Team Member testing

--- test/TeamMember.ts
+++ test/TeamMember.ts
@@ -52,6 +52,38 @@ describe('TeamMember', () => {
         teamMember.DoWork(backlog, clock);
         expect(backlog.IsCompleted).to.equal(true);
     }),
+    it('Team Member can not complete work as there is prerequisite', () => {
+        let teamGraph = [
+            [0]
+        ]
+        let membersConfig = [
+                new MemberConfig("DEV", 1, 1, 1/2)
+        ]
+        let backlogConfig = new BacklogConfig(2, 1, 0, 1, 0);
+        let backlog = Backlog.Generate(membersConfig, backlogConfig);
+        let clock = new Clock(1);
+
+        let teamMember = new TeamMember(0, membersConfig[0], teamGraph);
+        teamMember.DoWork(backlog, clock);
+        expect(backlog.IsCompleted).to.equal(false);
+    }),
+    it('Team Member can  complete work as  prerequisite is completed', () => {
+        let teamGraph = [
+            [0]
+        ]
+        let membersConfig = [
+                new MemberConfig("DEV", 1, 1, 1/2)
+        ]
+        let backlogConfig = new BacklogConfig(2, 1, 0, 1, 0);
+        let backlog = Backlog.Generate(membersConfig, backlogConfig);
+        let clock = new Clock(1);
+
+        let teamMember = new TeamMember(0, membersConfig[0], teamGraph);
+        teamMember.DoWork(backlog, clock);
+        teamMember.DoWork(backlog, clock);
+        expect(backlog.IsCompleted).to.equal(true);
+    }),
+
     it('Team Member can not do the work as there is dependency upstream', () => {
         let teamGraph = [
             [0, 0],
@@ -91,6 +123,27 @@ describe('TeamMember', () => {
         DevTeamMember.DoWork(backlog, clock);
 
         expect(backlog.IsCompleted).to.equal(true);
+    }),
+    it('DEV found a problem and gave feedback to the PO, work is not completed', () => {
+        let teamGraph = [
+            [0, 1],
+            [1, 0]
+        ];
+        let membersConfig = [
+                new MemberConfig("PO", 1, 1, 1/2),
+                new MemberConfig("DEV", 1, 1, 1/2)
+        ]
+        let backlogConfig = new BacklogConfig(1, 0, 0, 5, 0);
+        let backlog = Backlog.Generate(membersConfig, backlogConfig);
+        let clock = new Clock(5);
+
+        let POTeamMember = new TeamMember(0, membersConfig[0], teamGraph);
+        POTeamMember.DoWork(backlog, clock);
+        let DevTeamMember = new TeamMember(1, membersConfig[1], teamGraph);
+        DevTeamMember.DoWork(backlog, clock);
+
+        expect(backlog.IsCompleted).to.equal(false);
+        expect(backlog.Find(0).Tasks[0].Actual > backlog.Find(0).Tasks[0].Original).to.equal(true);
     })
 
 });
