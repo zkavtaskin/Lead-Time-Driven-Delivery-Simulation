953804418c591e3d6b440cf7cf6f2262b9ee2c48
Zan Kavtaskin <kavtaskinz@MSL2487.midland-software.co.uk>
2020-05-25 00:04:08 +0100

    team member sort

--- OptimisationTest/GeneticDecoderBacklog.ts
+++ OptimisationTest/GeneticDecoderBacklog.ts
@@ -57,6 +57,25 @@ describe('GeneticDecoderBacklog', () => {
         const actual = stories.sort(lambda).map(story => story.Id);
         const expected = [4, 1, 0, 3, 2, 5];
         expect(actual).to.eql(expected)
+    }),
+
+    it('Decode, stories are delivered by 2 team members, gene is set to prioritise by single team member only i.e. in reverse', () => {
+
+        const storyA = new Story(0, false, null, Array<Task>(new Task(7), new Task(0)));
+        const storyB = new Story(1, false, null, Array<Task>(new Task(6), new Task(0)));
+        const storyC = new Story(2, false, null, Array<Task>(new Task(5), new Task(0)));
+        const storyD = new Story(3, false, null, Array<Task>(new Task(4), new Task(0)));
+        const storyE = new Story(4, false, null, Array<Task>(new Task(3), new Task(0)));
+        const storyF = new Story(5, false, null, Array<Task>(new Task(2), new Task(0)));
+        const stories = new Array<Story>(storyA, storyB, storyC, storyD, storyE, storyF);
+
+        const teamConfig = new TeamConfig([new MemberConfig("", 1, 1, 1), new MemberConfig("", 1, 1, 1)],  null);
+        const decoder = new GeneticDecoderBacklog(teamConfig);
+        const lambda = decoder.Decode([0, 0, 0, 1, 0, 0, 0, 0, 0]);
+        const actual = stories.sort(lambda).map(story => story.Id);
+        const expected = [5, 4, 3, 2, 1, 0];
+        expect(actual).to.eql(expected)
     })
+
 });
     
