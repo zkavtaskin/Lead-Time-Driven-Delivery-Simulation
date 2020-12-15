31e5b7c0b780dbb500622db4b1359a4fc65d188d
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2020-12-15 13:25:50 +0000

    branch and bound

--- Optimisation/BranchBound/BnBBacklog.ts
+++ Optimisation/BranchBound/BnBBacklog.ts
@@ -2,21 +2,35 @@ import { BacklogConfig } from "../../Simulation/BacklogConfig";
 import { TeamConfig } from "../../Simulation/TeamConfig";
 import { Result } from "../Result"
 import { BacklogOptimiser } from "../BacklogOptimiser";
+import { BnBDecoderBacklog } from "./BnBDecoderBacklog";
 
 export class BnBBacklog implements BacklogOptimiser {
 
     private readonly teamConfig : TeamConfig;
     private readonly backlogConfig : BacklogConfig;
     private readonly effortSize : number;
+    private readonly decoder : BnBDecoderBacklog;
 
     constructor(teamConfig : TeamConfig, backlogConfig : BacklogConfig, effortSize : number = 0.5) {
         this.teamConfig = teamConfig;
         this.backlogConfig = backlogConfig;
         this.effortSize = effortSize;
+        this.decoder = new BnBDecoderBacklog(teamConfig);
     }
 
     Solve() : Result {
+        const depth = this.decoder.Size;
+        let patterns = []
+        for(let i = 0; i < depth; i++) {
+            //TODO
+        }
+        
+
         return null;
+    }
+
+    private factorial(n:number) {
+        return n ? n * this.factorial(n - 1) : 1;
     }
 
 }
---
+++ Optimisation/BranchBound/BnBDecoderBacklog.ts
@@ -0,0 +1,79 @@
+import { TeamConfig } from "../../Simulation/TeamConfig";
+import { Story } from "../../Simulation/Story";
+
+export class BnBDecoderBacklog {
+
+    get Size() : number {
+        return this.decodeMap.size;
+    }
+
+    private decodeMap = new Map<number, (a : Story, b : Story) => number>();
+    private decodeReadableMap = new Map<number, string>();
+
+    constructor(teamConfig : TeamConfig) {
+
+        this.decodeMap.set(0, this.orderByPrerequisite);
+        this.decodeReadableMap.set(0, "PrerequisiteId");
+
+        this.decodeMap.set(1, this.orderBySizeLargest);
+        this.decodeReadableMap.set(1, "OrderByLargest");
+
+        this.decodeMap.set(2, this.orderBySizeSmallest);
+        this.decodeReadableMap.set(2, "OrderBySmallest");
+
+        for(let teamMemberId = 0; teamMemberId < teamConfig.Members.length; teamMemberId++) {
+            this.decodeMap.set(teamMemberId + 3, (a, b) => this.orderByTeamMemberVolumeOfWork(teamMemberId, a, b));
+            this.decodeReadableMap.set(teamMemberId + 3, teamConfig.Members[teamMemberId].Name);
+        }
+    }
+
+    public Decode(pattern : Array<number>) : (a : Story, b : Story) => number {
+        const lambda = (a : Story, b : Story, index : number) => {  
+            if(pattern.length >= index) return;
+            return this.decodeMap.get(pattern[index])(a, b) || lambda(a, b, index+1);
+        }
+       return (a, b) => lambda(a, b, 0);
+    }
+
+    public DecodeReadable(pattern : Array<number>) : Array<string> {
+        const decoded = new Array<string>();
+        for(let i = 0; i < pattern.length; i++) {
+            decoded.push( this.decodeReadableMap.get(pattern[i]));
+        }
+        return decoded;
+    }
+
+    private orderBySizeLargest(a : Story, b : Story) : number {
+        return a.SizeOriginal-b.SizeOriginal; 
+    }
+
+    private orderBySizeSmallest(a : Story, b : Story) : number {
+        return b.SizeOriginal-a.SizeOriginal; 
+    }
+
+    private orderByPrerequisite(a : Story, b : Story) : number {
+        if(!a.HasPrerequisite() && !b.HasPrerequisite())
+            return 0;
+
+        if(!a.HasPrerequisite())
+            return -1;
+        
+        if(!b.HasPrerequisite())
+            return 1;
+
+        return a.PrerequisiteId-b.PrerequisiteId; 
+    }
+
+    private orderByTeamMemberVolumeOfWork(teamMemberId : number, a : Story, b : Story) : number {
+        if(a.Tasks[teamMemberId] == null && b.Tasks[teamMemberId] == null)
+        return 0;
+
+        if(a.Tasks[teamMemberId] == null)
+            return -1;
+
+        if(b.Tasks[teamMemberId] == null)
+            return 1;
+
+        return a.Tasks[teamMemberId].Remaining-b.Tasks[teamMemberId].Remaining; 
+    }
+}
