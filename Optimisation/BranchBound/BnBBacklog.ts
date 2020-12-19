e3e47dc1d820c35a1fc60dae790a0ba8fd2f16bb
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2020-12-19 23:13:41 +0000

    search relaxation

--- Optimisation/BranchBound/BnBBacklog.ts
+++ Optimisation/BranchBound/BnBBacklog.ts
@@ -20,40 +20,48 @@ export class BnBBacklog implements BacklogOptimiser {
     }
 
     Solve() : Result {
-        let best = Infinity, bestPattern = null;
+        let min = Infinity,  bestPattern = null;
         const teamSimulation = new TeamSimulation(null, this.teamConfig, this.backlogConfig, this.effortSize);
-        let boundary = (pattern) => {
+        let valuation = (pattern) => {
             teamSimulation.Reset(this.decoder.Decode(pattern));
-            const teamSimulationStats = teamSimulation.Run().GetStats();
-            if(teamSimulationStats.LeadTime.Mean < best) {
-                best = teamSimulationStats.LeadTime.Mean;
+            const mean = teamSimulation.Run().GetStats().LeadTime.Mean;
+            if(mean < min) {
+                min = mean;
                 bestPattern = pattern;
-                return true
             }
-            return false
+            if(mean/min < 1.05)
+                return mean;
+                
+            return null;
         }
-        BnBBacklog.generateCombinations(this.decoder.Size, boundary)
-        return new Result(best, bestPattern, this.decoder.DecodeReadable(bestPattern));
+        const results = BnBBacklog.generateCombinations(this.decoder.Size, valuation);
+        return new Result(min, bestPattern, this.decoder.DecodeReadable(bestPattern));
     }
 
-    static generateCombinations(n:number, boundary:(p : Array<number>) => boolean) {
+    static generateCombinations(n:number, valuator:(pattern : Array<number>) => number) {
         const bagOrigin:Array<number> = [...Array(n).keys()];
         const root = [[[...bagOrigin], []]];
-        const combinations = []
+        const combinations = [];
+        const valuations = [];
         let bag = null;
+        let valuation:number;
         while(bag = root.shift()) {
             for(let i:number = 0; i < bag[0].length; i++) {
                 const newBag = [...bag[0]];
                 const newPattern = [...bag[1]];
                 const k = newBag.splice(i, 1)[0];
                 newPattern.push(k);
-                if(boundary == undefined || boundary(newPattern)) {
-                    root.push([newBag, newPattern]);
-                    combinations.push(newPattern)
+                if(valuator) {
+                    if(valuation = valuator(newPattern))
+                        valuations.push(valuation);
+                    else 
+                        continue;
                 }
+                root.push([newBag, newPattern]);
+                combinations.push(newPattern);
             }
         } 
-        return combinations;
+        return [combinations, valuations];
     }
 
 }
