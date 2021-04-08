0b0ad39bd1feef705229fc3d066c8e080409a920
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2021-04-08 20:42:09 +0100

    testing new lib

--- Optimisation/Continuous/MemberCapacityOptimiser.ts
+++ Optimisation/Continuous/MemberCapacityOptimiser.ts
@@ -2,9 +2,9 @@ import { TeamConfig } from "../../Simulation/TeamConfig";
 import { TeamSimulation } from "../../Simulation/TeamSimulation";
 import { BacklogConfig } from "../../Simulation/BacklogConfig";
 import { ContinuousResult } from "../Continuous/ContinuousResult"
-import * as jsregression from '../../node_modules/js-regression/src/jsregression.js'
 import { Statistics } from "../../Simulation/Statistics";
-import { mode } from "simple-statistics";
+import MLR from "ml-regression-multivariate-linear"
+// import optimize from "optimization-js"
 
 
 /* 
@@ -25,16 +25,15 @@ export class MemberCapacityOptimiser  {
     }   
 
     Optimise() : ContinuousResult {
-        let Xy = Array<Array<number>>(), X = Array<Array<number>>();
-        const model = jsregression.LinearRegression();
+        let X = Array<Array<number>>(), Y = Array<Array<number>>();
         let rmsePrevious = null;
         while(true) {
             const samples = this.secondDegreeBatchSamples(1);
             X = X.concat(samples[0]);
-            Xy = Xy.concat(samples[1]);
-            model.fit(Xy);
-            const yPredicted = model.transform(X);
-            const rmse = Math.sqrt(Statistics.MeanSquaredError(Xy.map(x => x[Xy.length-1]), yPredicted));
+            Y = Y.concat(samples[1]);
+            const regression = new MLR(X, Y);
+            const yPredicted = regression.predict(X);
+            const rmse = Math.sqrt(Statistics.MeanSquaredError(Y.map(y => y[0]), yPredicted.map(y => y[0])));
             //gains check
             if((rmsePrevious-rmse)/rmsePrevious < 0.01) {
                 break;
@@ -44,17 +43,17 @@ export class MemberCapacityOptimiser  {
 
         //5. Once error is stable find optimal y using gradient descent 
         //return optimal x
-        return new ContinuousResult([]);
+        return new ContinuousResult([0]);
     }
 
     private secondDegreeBatchSamples(nBatches : number) : [Array<Array<number>>, Array<Array<number>>] {
-        const Xy = Array<Array<number>>(), X =  Array<Array<number>>();
+        const X = Array<Array<number>>(), Y =  Array<Array<number>>();
         for(let i = 0; i < nBatches * (32 * this.teamConfig.Members.length); i++){
             const sample = this.secondDegreeSample();
             X.push(sample[0]);
-            Xy.push(sample[1]);
+            Y.push(sample[1]);
         }
-        return [X, Xy];
+        return [X, Y];
     }
 
     private secondDegreeSample() : [Array<number>, Array<number>] {
@@ -62,12 +61,11 @@ export class MemberCapacityOptimiser  {
         const teamConfigSample = this.teamConfig.ChangeMembersCapacity(this.teamConfig.Members.map((m) => (Math.random() * 5) * m.Capacity));
         const teamSimulation = new TeamSimulation(teamConfigSample, this.backlogConfig, this.effortSize);
         const teamMetrics = teamSimulation.Run();
-        const y1 = teamMetrics.Backlog.LeadTime.Max;
-        const y2 = teamMetrics.Members.reduce((s, m) => s + m.TimeIdle.Median, 0);
-        const y = Math.log10(y1 + y2);
+        const Y1 = teamMetrics.Backlog.LeadTime.Max;
+        const Y2 = teamMetrics.Members.reduce((s, m) => s + m.TimeIdle.Median, 0);
+        const Y = Math.log10(Y1 + Y2);
         const X = Statistics.TransformTo2ndDegreePolynomial(teamConfigSample.Members.map((m) => m.Capacity));
-        const Xy = X.concat(y);
-        return [X, Xy];
+        return [X, [Y]];
     }
 
 }
