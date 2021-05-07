7a8c5493b4fa98f8e93898b0ba53ba655df80876
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2021-05-07 23:32:29 +0100

    format

--- main.ts
+++ main.ts
@@ -16,6 +16,9 @@ Experiments search for:
     2) Follows as close uniform distribution as possible  
     3) Delivers biggest amount of stories (value), this is a given as under the experiment everyone needs to deliver set amount of stories.  
 As a starting point experiment will be setup to have a bias towards delivering work right at the end the cycle.
+
+All lower ▼ the better. 
+
 `);
 
 experiments.forEach((experiment) => {
@@ -41,18 +44,18 @@ ${resultView(results.Experiment, results.EffortPerTick)}
 });
 
 function resultView(result : Result, tickSize : number) {
-  return `Total mean man-days: original ${Statistics.ToDecimalPlace(result.WorkSizeOriginalMean)}, actual ${Statistics.ToDecimalPlace(result.WorkSizeActualMean)}
+  return `  Total mean man-days: original ${Statistics.ToDecimalPlace(result.WorkSizeOriginalMean)}, actual ${Statistics.ToDecimalPlace(result.WorkSizeActualMean)}
 Conditions: 
-${result.Conditions.map((c) => c[0] + " : " + c[1]).join("\n")}
+${result.Conditions.map((c) => "  " + c[0] + " : " + c[1]).join("\n")}
 ## Lead Time 
-  Uniformity Deviation: ${result.LeadTime.Frequency}, Skew: ${result.LeadTime.Skew}
+  Days Deviation: ${result.LeadTime.Frequency}
 *When* delivered: 
   First 25% delivered on day ${result.LeadTime.Quartiles[0] * tickSize}, 50% ${result.LeadTime.Quartiles[1] * tickSize}, 75% ${result.LeadTime.Quartiles[2] * tickSize}, last 25% ${result.LeadTime.Max * tickSize}
 ## Cycle Time
-*Time taken* to deliver once started: 
+  *Time taken* to deliver once started: 
   25% has taken ${result.CycleTime.Quartiles[0] * tickSize} day(s), 50% ${result.CycleTime.Quartiles[1] * tickSize}, 75% ${result.CycleTime.Quartiles[2] * tickSize}, last 25% ${result.CycleTime.Max * tickSize}
 ## Constraint
-  Member: ${result.Constraint}, Uniformity Deviation: ${Statistics.ToDecimalPlace(result.TeamMembersIdleFrequency)}
+  Member: ${result.Constraint}, Idle Deviation: ${Statistics.ToDecimalPlace(result.TeamMembersIdleFrequency)}
 ## Team Members
 ${result.TeamMembers.reduce((s,m) => s+(" "+ m.Name + " => idle days " + Statistics.ToDecimalPlace(m.TimeIdle.Median,0) + ", turn count: waiting " + Statistics.ToDecimalPlace(m.SkipNotMyTurn.Median,1) + ", preq " + Statistics.ToDecimalPlace(m.SkipPrerequisite.Median,1) + ", feedback " + Statistics.ToDecimalPlace(m.GivenFeedback.Median,1) + "\n"), "")}`;
 }
